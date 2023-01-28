083c0cb5d29b1e47598487f77828554c9065c65a
Eric <menaericdaniel@gmail.com>
2023-01-28 02:58:53 -0300

    create helpers & methods Auth

--- database/config.js
+++ database/config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-
+        await mongoose.set('strictQuery', false)
         const {connection} =  await mongoose.connect(process.env.DB_CONNECTION)
         //console.log(connection)
         const url = `${connection.host}:${connection.port}`
