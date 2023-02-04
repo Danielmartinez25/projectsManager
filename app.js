1677d3d80eedb0c4d094dc841dd0992ac1177306
Eric <menaericdaniel@gmail.com>
2023-02-04 07:44:07 -0300

    add validations & public resources in server

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app
   .use(logger('dev'))
   .use(express.json())
   .use(express.urlencoded({ extended: false }))
+  .use(express.static('./public'))
   .use(cors())
 
   /* RUTAS */
--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -1,160 +1,155 @@
 const createError = require("http-errors");
 const Project = require("../database/models/Project");
 const errorResponse = require("../helpers/errorResponse");
-const ObjectId = require("mongoose").Types.ObjectId
+const ObjectId = require("mongoose").Types.ObjectId;
 
 module.exports = {
-    list : async (req,res) => {
-        try {
+  list: async (req, res) => {
+    try {
+      const projects = await Project.find().where("createdBy").equals(req.user);
 
-            const projects = await Project.find().where('createdBy').equals(req.user)
+      return res.status(200).json({
+        ok: true,
+        msg: "Lista de Proyectos",
+        projects,
+      });
+    } catch (error) {
+      console.log(error);
+      return errorResponse(res, error, "LOGIN");
+    }
+  },
+  store: async (req, res) => {
+    try {
+      const { name, description, client } = req.body;
 
-            return res.status(200).json({
-                ok : true,
-                msg :'Lista de Proyectos',
-                projects
-            });
+      if (
+        [name, description, client].includes("") ||
+        !name ||
+        !description ||
+        !client
+      )
+        throw createError(400, "Todos los campos son obligatorios");
 
-        } catch (error) {
-            console.log(error);
-            return errorResponse(res,error, "LOGIN")
+      if (!req.user) throw createError(401, "Error de autenticación");
 
-        }
+      const project = new Project(req.body);
+      project.createdBy = req.user._id;
 
-    },
-    store : async (req,res) => {
-        try {
+      //console.log(project)
+      const projectStore = await project.save();
 
-            const {name, description ,client} = req.body;
+      return res.status(201).json({
+        ok: true,
+        msg: "Proyecto guardado",
+        project: projectStore,
+      });
+    } catch (error) {
+      console.log(error);
+      return errorResponse(res, error, "LOGIN");
+    }
+  },
+  detail: async (req, res) => {
+    try {
+      const { id } = req.params;
+      if (!ObjectId.isValid(id)) throw createError(400, "No es un ID válido");
 
-            if([name, description, client].includes("") || !name || !description || !client) throw createError(400,"Todos los campos son obligatorios");
+      const project = await Project.findById(id);
 
-            if(!req.user)throw createError(401,"Error de autenticación");
+      if (!project) throw createError(404, "Proyecto no encontrado");
 
-            const project = new Project(req.body);
-            project.createdBy = req.user._id;
+      if (req.user._id.toString() !== project.createdBy.toString())
+        throw createError(401, "No estás autorizado/a");
 
-            //console.log(project)
-            const projectStore = await project.save()
+      return res.status(200).json({
+        ok: true,
+        msg: "Detalle del Proyecto",
+        project,
+      });
+    } catch (error) {
+      console.log(error);
+      return errorResponse(res, error, "LOGIN");
+    }
+  },
+  update: async (req, res) => {
+    try {
+      const { id } = req.params;
+      if (!ObjectId.isValid(id)) throw createError(400, "No es un ID válido");
 
-            return res.status(201).json({
-                ok : true,
-                msg :'Proyecto guardado',
-                project : projectStore
-            })
-        } catch (error) {
-            console.log(error);
-            return errorResponse(res,error, "LOGIN")
+      const project = await Project.findById(id);
 
-        }
-       
-    },
-    detail : async (req,res) => {
-        try {
-            const {id} = req.params;
-            if(!ObjectId.isValid(id)) throw createError(400, "No es un ID válido");
+      if (!project) throw createError(404, "Proyecto no encontrado");
 
-            const project = await Project.findById(id);
-            
-            if(!project) throw createError(404, "Proyecto no encontrado");
+      if (req.user._id.toString() !== project.createdBy.toString())
+        throw createError(401, "No estás autorizado/a");
 
-            if(req.user._id.toString() !== project.createdBy.toString()) throw createError(401, "No estás autorizado/a" );
+      const { name, description, client, dataExpire } = req.body;
 
-            return res.status(200).json({
-                ok : true,
-                msg :'Detalle del Proyecto',
-                project
-            })
-        } catch (error) {
-            console.log(error);
-            return errorResponse(res,error, "LOGIN")
+      project.name = name || project.name;
+      project.description = description || project.description;
+      project.client = client || project.client;
+      project.dataExpire = dataExpire || project.dataExpire;
 
-        }
-       
-    },
-    update : async (req,res) => {
-        try {
+      const projecUpdated = await project.save();
 
-            const {id} = req.params;
-            if(!ObjectId.isValid(id)) throw createError(400, "No es un ID válido");
+      return res.status(201).json({
+        ok: true,
+        msg: "Proyecto actualizado",
+        project: projecUpdated,
+      });
+    } catch (error) {
+      console.log(error);
+      return errorResponse(res, error, "LOGIN");
+    }
+  },
+  remove: async (req, res) => {
+    try {
+      const { id } = req.params;
+      if (!ObjectId.isValid(id)) throw createError(400, "No es un ID válido");
 
-            const project = await Project.findById(id);
-            
-            if(!project) throw createError(404, "Proyecto no encontrado");
+      const project = await Project.findById(id);
 
-            if(req.user._id.toString() !== project.createdBy.toString()) throw createError(401, "No estás autorizado/a" );
+      if (!project) throw createError(404, "Proyecto no encontrado");
 
-            const {name, description ,client, dataExpire} = req.body;
-        
-            project.name = name || project.name;
-            project.description = description || project.description;
-            project.client = client || project.client;
-            project.dataExpire = dataExpire || project.dataExpire;
+      if (req.user._id.toString() !== project.createdBy.toString())
+        throw createError(401, "No estás autorizado/a");
 
-            const projecUpdated = await project.save()
+      await project.deleteOne();
 
-            return res.status(201).json({
-                ok : true,
-                msg :'Proyecto actualizado',
-                project : projecUpdated
-            });
-
-        } catch (error) {
-            console.log(error);
-            return errorResponse(res,error, "LOGIN")
-        }
-    },
-    remove : async (req,res) => {
-        try {
-
-            const {id} = req.params;
-            if(!ObjectId.isValid(id)) throw createError(400, "No es un ID válido");
-
-            const project = await Project.findById(id);
-            
-            if(!project) throw createError(404, "Proyecto no encontrado");
-
-            if(req.user._id.toString() !== project.createdBy.toString()) throw createError(401, "No estás autorizado/a" );
-
-            await project.deleteOne();
-
-            return res.status(200).json({
-                ok : true,
-                msg :'Proyecto eliminado con éxito'
-            })
-        } catch (error) {
-            console.log(error);
-            return errorResponse(res,error, "LOGIN")
-
-        }
-    },
-    addCollaborator : async (req,res) => {
-        try {
-            return res.status(200).json({
-                ok : true,
-                msg :'Colaborador agregado'
-            })
-        } catch (error) {
-            console.log(error);
-            return res.status(error.status || 500).json({
-                ok : false,
-                msg : error.message || 'Upss, hubo un error en COLLABORATOR-ADD'
-            })
-        }
-    },
-    removeCollaborator : async (req,res) => {
-        try {
-            return res.status(200).json({
-                ok : true,
-                msg :'Colaborador eliminado'
-            })
-        } catch (error) {
-            console.log(error);
-            return res.status(error.status || 500).json({
-                ok : false,
-                msg : error.message || 'Upss, hubo un error en COLLABORATOR-REMOVE'
-            })
-        }
-    },
-
-}
+      return res.status(200).json({
+        ok: true,
+        msg: "Proyecto eliminado con éxito",
+      });
+    } catch (error) {
+      console.log(error);
+      return errorResponse(res, error, "LOGIN");
+    }
+  },
+  addCollaborator: async (req, res) => {
+    try {
+      return res.status(200).json({
+        ok: true,
+        msg: "Colaborador agregado",
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(error.status || 500).json({
+        ok: false,
+        msg: error.message || "Upss, hubo un error en COLLABORATOR-ADD",
+      });
+    }
+  },
+  removeCollaborator: async (req, res) => {
+    try {
+      return res.status(200).json({
+        ok: true,
+        msg: "Colaborador eliminado",
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(error.status || 500).json({
+        ok: false,
+        msg: error.message || "Upss, hubo un error en COLLABORATOR-REMOVE",
+      });
+    }
+  },
+};
