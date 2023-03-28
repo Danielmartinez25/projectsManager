144985aae6a2cfa3d545b6ac25806a8701e55af0
Danimartinez25 <danimartinez1325@gmail.com>
2023-03-28 18:30:22 -0300

    confirm acount

--- client/src/pages/ConfirmAccount.jsx
+++ client/src/pages/ConfirmAccount.jsx
@@ -53,7 +53,6 @@ export const ConfirmAccount = () => {
         Confirma tu cuenta
       </h1>
       <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded bg-white'>
-
       {
       alert.msg && (
         <>
